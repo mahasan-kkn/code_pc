efeb633f82493587e34236e0915e4dd861a5b41b
mahasan-kkn <alongkorn.meesin@gmail.com>
2020-12-18 16:21:42 +0700

    huay

--- tree.py
+++ tree.py
@@ -1,9 +1,15 @@
 import random
 print("เลือกเลขมา 6 ตัวที่ไม่ซ้ำ")
 b = []
-for i in range(6):
+for i in range(6): # วนเก็บข้อมูล 6 ครั้งเก็บไว้ในตัวแปร b
     print(f"เลขตัวที่ {i+1} : ")
-    a = input()
-    b.append(a)
+    while True:
+        a = input()
+        if len(a) > 1 or a.isdigit() != True or a in b : # ตรวจสอบให้กรอกทีละตัว,เป็นตัวเลข และไม่ซ้ำตัวที่เคยกรอกมาแล้ว
+            print("ไม่ถูกต้องพิมพ์ใหม่")
+            continue
+        else:
+            b.append(a) # ตรงตามเงื่อนไขก็เพิ่มข้อมูลเก็บไว้ในตัวแปร b
+            break
 print(b)
 
