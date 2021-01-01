fa47f73d4cd29aedf01abc3e7e0bb5c3a42f5f71
mahasan-kkn <alongkorn.meesin@gmail.com>
2021-01-02 05:07:39 +0700

    private

---
+++ private.py
@@ -0,0 +1,5 @@
+# token id = "d3dVYwoTtmtGtLrDGXz491ZZlzogquSTAgVAa0bF7bI"
+from parinya import LINE
+line = LINE("d3dVYwoTtmtGtLrDGXz491ZZlzogquSTAgVAa0bF7bI")
+#line.sendtext("hello you")
+line.sendsticker(1,1)
